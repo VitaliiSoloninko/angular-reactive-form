c1c9378ede0aba8c9bdf523472f07ce3ae8cf9a5
22051988Di <vitalii.soloninko@gmail.com>
2024-09-15 12:49:47 +0200

    2	Login - FormGroup and FormControl

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+	selector: 'app-login',
+	standalone: true,
+	templateUrl: './login.component.html',
+	styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+	form = new FormGroup({
+		email: new FormControl(''),
+		password: new FormControl(''),
+	})
+
+	onSubmit() {}
+}
