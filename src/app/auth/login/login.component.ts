d033c6008c85b5c0d19c0eb8d7cf6894022f49df
22051988Di <vitalii.soloninko@gmail.com>
2024-09-17 18:57:56 +0200

    7.1	Form Programmatically (pre-populate) save to localStorage

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, DestroyRef, inject, OnInit } from '@angular/core'
 import {
 	AbstractControl,
 	FormControl,
@@ -6,7 +6,7 @@ import {
 	ReactiveFormsModule,
 	Validators,
 } from '@angular/forms'
-import { of } from 'rxjs'
+import { debounceTime, of } from 'rxjs'
 
 function mustContainQuestionMark(control: AbstractControl) {
 	if (control.value.includes('?')) {
@@ -29,7 +29,8 @@ function emailIsUnique(control: AbstractControl) {
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+	private destroyRef = inject(DestroyRef)
 	form = new FormGroup({
 		email: new FormControl('', {
 			validators: [Validators.email, Validators.required],
@@ -59,6 +60,20 @@ export class LoginComponent {
 			this.form.controls.password.dirty &&
 			this.form.controls.password.invalid
 		)
+	}
+
+	ngOnInit() {
+		const subscription = this.form.valueChanges
+			.pipe(debounceTime(500))
+			.subscribe({
+				next: value => {
+					window.localStorage.setItem(
+						'saved-login-form',
+						JSON.stringify({ email: value.email })
+					)
+				},
+			})
+		this.destroyRef.onDestroy(() => subscription.unsubscribe())
 	}
 
 	onSubmit() {
