c9eda42ebfde9defe165b1cbd9fbba6b60561580
22051988Di <vitalii.soloninko@gmail.com>
2024-09-18 11:20:05 +0200

    9	Form Sign Up: email, password, reset, submit

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import {
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms'
 
 @Component({
-  selector: 'app-signup',
-  standalone: true,
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css',
+	selector: 'app-signup',
+	standalone: true,
+	imports: [ReactiveFormsModule],
+	templateUrl: './signup.component.html',
+	styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+	form = new FormGroup({
+		email: new FormControl('', {
+			validators: [Validators.email, Validators.required],
+		}),
+		password: new FormControl('', {
+			validators: [Validators.required, Validators.minLength(6)],
+		}),
+		confirmPassword: new FormControl('', {
+			validators: [Validators.required, Validators.minLength(6)],
+		}),
+	})
+
+	onSubmit() {
+		const enteredEmail = this.form.value.email
+		const enteredPassword = this.form.value.password
+		console.log(enteredEmail, enteredPassword)
+	}
+
+	onReset() {
+		this.form.reset()
+	}
+}
